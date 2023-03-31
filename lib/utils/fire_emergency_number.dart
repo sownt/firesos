bc03967b91b347b10fe98090feec548e603fb6f6
sownt <thonsai@outlook.com>
2023-03-31 23:16:43 +0700

    add translations

---
+++ lib/utils/fire_emergency_number.dart
@@ -0,0 +1,7 @@
+class FireEmergencyNumber {
+  /// Using abbreviated country name, according to the two letter (alpha-2) [ISO standard](https://www.iso.org/iso-3166-country-codes.html).
+  static final phones = {
+    'US': '911',
+    'VN': '114',
+  };
+}
